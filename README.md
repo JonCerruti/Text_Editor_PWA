0a77bf5d8caa4bab7767347cb57285e54a69aa60
Jon <jonathancerruti@gmail.com>
2022-10-07 00:54:38 -0700

    pushing to heroku

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Text Editor Starter Code
+https://git.heroku.com/enigmatic-earth-01059.git
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -33,6 +33,6 @@ butInstall.addEventListener('click', async () => {
 // Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
 
-     // Clear prompt
+    
   window.deferredPrompt = null;
 });
